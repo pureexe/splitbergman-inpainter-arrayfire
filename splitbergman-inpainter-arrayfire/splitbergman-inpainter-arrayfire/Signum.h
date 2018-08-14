08b204e1226065e5f05f18a19dfe76e6afea561a
Pakkapon Phongthawee <me@pureapp.in.th>
2018-08-14 16:01:09 +0700

    remove: unuse variable in signum.h

--- splitbergman-inpainter-arrayfire/splitbergman-inpainter-arrayfire/Signum.h
+++ splitbergman-inpainter-arrayfire/splitbergman-inpainter-arrayfire/Signum.h
@@ -2,10 +2,7 @@
 #include "stdafx.h"
 
 array Signum(array input) {
-	int height = input.dims(0);
-	int width = input.dims(1);
 	array positive = (input > 0) * 1.0;
 	array negative = (input < 0) * -1.0;
-	array result = constant(0,height,width);
-	return result + positive + negative;
+	return  positive + negative;
 }
